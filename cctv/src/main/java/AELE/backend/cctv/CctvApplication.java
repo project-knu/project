d564cf8d2795e0982fe3a64d0af676e67f876095
dbwlsl <716yujin@gmail.com>
2024-11-22 16:40:21 +0900

    Modify logo title

--- cctv/src/main/java/AELE/backend/cctv/CctvApplication.java
+++
@@ -1,9 +0,0 @@
-package AELE.backend.cctv;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@SpringBootApplication
-@EnableJpaAuditing
-public class CctvApplication { public static void main(String[] args) {SpringApplication.run(CctvApplication.class, args);}}
