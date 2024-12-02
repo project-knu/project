1c9540256af30eab12aced3028bc13f57946b2b3
jinuklee <150532431+jinuk0211@users.noreply.github.com>
2024-12-02 14:37:56 +0900

    Update llama3B.py

---
+++ api_ai/llama3B.py
@@ -0,0 +1,11 @@
+#llama 3B
+from transformers import pipeline
+
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
+pipe = pipeline(
+    "text-generation",
+    model=model_id,
+    torch_dtype=torch.bfloat16,
+    device_map="auto",
+)
+#llama 7B 추가사항
